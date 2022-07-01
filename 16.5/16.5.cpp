465ec3e1737066b1fbbf83ddc1c6f13875259bbc
Ian <80196814+Screll3@users.noreply.github.com>
2022-07-01 16:15:05 +0700

    16.5
    V 1.0

---
+++ 16.5/16.5.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <time.h>
+
+int main()
+{
+    const int n = 13;
+    int a[n][n];
+
+
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            a[i][j] = i + j;
+
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+            std::cout << a[i][j] << " ";
+        std::cout << "\n";
+    }
+
+
+    struct tm buf;
+    time_t t = time(NULL);
+    localtime_s(&buf, &t);
+
+    std::cout << (a[buf.tm_mday % n][0]*2+n-1)/2*n;
+}
